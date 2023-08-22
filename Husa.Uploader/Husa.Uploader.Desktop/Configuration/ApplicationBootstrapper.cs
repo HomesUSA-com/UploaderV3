a826e6fa58d093b62ac3194c8cecf4a4842242c7
Marco Roa <marco.roa@gmail.com>
2023-08-22 17:56:02 +0000

    Merged in bugfix/HRP-2966-fix-chrome-browser-to-project (pull request #134)
    Bugfix/HRP-2966 fix chrome browser to project
    
    * Modified pipeline and project configuration to include a local version of chrome for testing with the deployable binary
    
    * Updated app version
    
    
    Approved-by: Gonzalo Rodulfo

--- Husa.Uploader/Husa.Uploader.Desktop/Configuration/ApplicationBootstrapper.cs
+++ Husa.Uploader/Husa.Uploader.Desktop/Configuration/ApplicationBootstrapper.cs
@@ -144,8 +144,8 @@ namespace Husa.Uploader.Desktop.Configuration
             services.AddTransient<IWebDriver>(provider =>
             {
                 var appOptions = provider.GetRequiredService<IOptions<ApplicationOptions>>().Value;
-                var chromeOptions = GetOptions(appOptions.Uploader);
                 var driverPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var chromeOptions = GetOptions(appOptions.Uploader, driverPath);
                 var driverService = ChromeDriverService.CreateDefaultService(driverPath);
                 driverService.HideCommandPromptWindow = true;
                 return new ChromeDriver(driverService, options: chromeOptions);
@@ -158,9 +158,12 @@ namespace Husa.Uploader.Desktop.Configuration
 
             return services;
 
-            static ChromeOptions GetOptions(UploaderSettings uploaderOptions)
+            static ChromeOptions GetOptions(UploaderSettings uploaderOptions, string driverPath)
             {
-                var options = new ChromeOptions();
+                var options = new ChromeOptions
+                {
+                    BinaryLocation = @$"{driverPath}\ChromeForTesting\chrome.exe",
+                };
                 options.AddArguments(uploaderOptions.ChromeOptions.Arguments);
                 foreach (var preference in uploaderOptions.ChromeOptions.UserProfilePreferences)
                 {
