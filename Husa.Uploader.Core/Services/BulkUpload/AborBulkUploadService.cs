7d6de08e3fe1c82588352dc20e2f94ae22df8802
andy_nunez <andy@homesusa.com>
2024-05-07 12:45:02 -0400

    HRP-6246: Bulk upload construction state changes abor.

--- Husa.Uploader.Core/Services/BulkUpload/AborBulkUploadService.cs
+++ Husa.Uploader.Core/Services/BulkUpload/AborBulkUploadService.cs
@@ -89,6 +89,9 @@ namespace Husa.Uploader.Core.Services.BulkUpload
         {
             switch (this.RequestFieldChange)
             {
+                case RequestFieldChange.ConstructionStage:
+                    await this.uploadService.UpdateStatus(bulkFullListing, cancellationToken, logInForCompany);
+                    break;
                 case RequestFieldChange.ListPrice:
                     await this.uploadService.UpdatePrice(bulkFullListing, cancellationToken, logInForCompany);
                     break;
