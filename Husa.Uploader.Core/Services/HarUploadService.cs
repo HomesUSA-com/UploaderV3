17ee30087a87b371b4c369c65266239d7f8746c3
Wladimir-husa <wladimir@homesusa.com>
2025-02-21 13:17:50 -0500

    fix acces market

--- Husa.Uploader.Core/Services/HarUploadService.cs
+++ Husa.Uploader.Core/Services/HarUploadService.cs
@@ -66,15 +66,12 @@ namespace Husa.Uploader.Core.Services
 
             var credentials = await LoginCommon.GetMarketCredentials(company, credentialsTask);
 
-            this.uploaderClient.NavigateToUrl(marketInfo.LogoutUrl);
+            this.uploaderClient.NavigateToUrl(marketInfo.LoginUrl);
             Thread.Sleep(2000);
-
-            this.uploaderClient.ExecuteScript(" jQuery('#ketch-consent-banner').find('button:first').click(); ");
+            this.uploaderClient.ScrollDown(400);
 
             // Connect to the login page
             var loginButtonId = "login_btn";
-            this.uploaderClient.NavigateToUrl(marketInfo.LoginUrl);
-            Thread.Sleep(1000);
             this.uploaderClient.WaitUntilElementIsDisplayed(By.Id(loginButtonId));
 
             this.uploaderClient.WriteTextbox(By.Id("username"), credentials[LoginCredentials.Username]);
