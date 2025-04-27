1a08014f9f564ad28756819aa0e9a79e752f49f2
andy_nunez <andy@homesusa.com>
2025-04-27 19:58:59 -0400

    HRP-2351: [Prod][All][UPL] Uploader - Show our MLS Dept the exact field which failed w/ brief explanation-qa
    Refactor to use UploaderResponse across services
    
    Replaced `UploadResult` with the new `UploaderResponse` model for consistency in methods throughout the project. Added detailed response information for operations including success, failure, and additional log data. Includes migration of services such as BulkUpload, Amarillo, and Ctx upload processes.

--- Husa.Uploader.Core/Interfaces/ServiceActions/IUploadListing.cs
+++ Husa.Uploader.Core/Interfaces/ServiceActions/IUploadListing.cs
@@ -1,7 +1,7 @@
 namespace Husa.Uploader.Core.Interfaces.ServiceActions
 {
     using Husa.Extensions.Common.Enums;
-    using Husa.Quicklister.Extensions.Domain.Enums;
+    using Husa.Uploader.Core.Models;
     using Husa.Uploader.Crosscutting.Enums;
     using Husa.Uploader.Data.Entities;
     using Husa.Uploader.Data.Entities.LotListing;
@@ -14,16 +14,16 @@ namespace Husa.Uploader.Core.Interfaces.ServiceActions
 
         bool CanUpload(ResidentialListingRequest listing);
 
-        Task<UploadResult> Upload(ResidentialListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true, bool autoSave = false);
+        Task<UploaderResponse> Upload(ResidentialListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true, bool autoSave = false);
 
-        Task<UploadResult> PartialUpload(ResidentialListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true, bool autoSave = false);
+        Task<UploaderResponse> PartialUpload(ResidentialListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true, bool autoSave = false);
 
         Task<LoginResult> Login(Guid companyId);
 
-        UploadResult Logout();
+        UploaderResponse Logout();
 
         void CancelOperation();
 
-        Task<UploadResult> UploadLot(LotListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true);
+        Task<UploaderResponse> UploadLot(LotListingRequest listing, CancellationToken cancellationToken = default, bool logIn = true);
     }
 }
