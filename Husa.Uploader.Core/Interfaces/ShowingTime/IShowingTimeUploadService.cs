be7a2792995a14e92f2bfe327ffd5916a3ed6294
Andrea Centeno <andrea@homesusa.com>
2025-07-17 16:18:49 +0000

    Merged in feature/HRP-9401 (pull request #1161)
    HRP-9401 update showing time
    
    * HRP-9401 update showing time
    
    * fix tests
    
    
    Approved-by: Israel Marquez
    Approved-by: Wladimir
    Approved-by: gonzalo

--- Husa.Uploader.Core/Interfaces/ShowingTime/IShowingTimeUploadService.cs
+++ Husa.Uploader.Core/Interfaces/ShowingTime/IShowingTimeUploadService.cs
@@ -1,7 +1,7 @@
 namespace Husa.Uploader.Core.Interfaces.ShowingTime
 {
     using Husa.Extensions.Common.Enums;
-    using Husa.Quicklister.Extensions.Api.Contracts.Models.ShowingTime;
+    using Husa.Quicklister.Extensions.Api.Contracts.Response.ShowingTime;
     using Husa.Quicklister.Extensions.Domain.Enums.ShowingTime;
     using Husa.Uploader.Core.Models;
     using Husa.Uploader.Data.Entities;
@@ -13,17 +13,17 @@ namespace Husa.Uploader.Core.Interfaces.ShowingTime
         Task<bool> GetInShowingTimeSite(Guid companyId, string mlsNumber, CancellationToken cancellationToken = default);
         Task SetAppointmentCenter(CancellationToken cancellationToken);
         Task SetAppointmentSettings(AppointmentType appointmentType, CancellationToken cancellationToken = default);
-        Task SetAppointmentRestrictions(AppointmentRestrictionsInfo info, CancellationToken cancellationToken = default);
-        Task SetAccessInformation(AccessInformationInfo info, CancellationToken cancellationToken = default);
-        Task SetAdditionalInstructions(AdditionalInstructionsInfo info, CancellationToken cancellationToken = default);
+        Task SetAppointmentRestrictions(AppointmentRestrictionsResponse info, CancellationToken cancellationToken = default);
+        Task SetAccessInformation(AccessInformationResponse info, CancellationToken cancellationToken = default);
+        Task SetAdditionalInstructions(AdditionalInstructionsResponse info, CancellationToken cancellationToken = default);
         Task SetDrivingDirections(ResidentialListingRequest request, CancellationToken cancellationToken = default);
-        Task<bool> AddExistentContact(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task<bool> AddNewContact(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task EditContact(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task SetContactConfirmSection(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task SetContactNotificationSection(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task SetContact(ContactDetailInfo contact, int position, CancellationToken cancellationToken = default);
-        Task SetContacts(IEnumerable<ContactDetailInfo> contacts, CancellationToken cancellationToken);
+        Task<bool> AddExistentContact(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task<bool> AddNewContact(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task EditContact(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task SetContactConfirmSection(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task SetContactNotificationSection(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task SetContact(ContactDetailResponse contact, int position, CancellationToken cancellationToken = default);
+        Task SetContacts(IEnumerable<ContactDetailResponse> contacts, CancellationToken cancellationToken);
         Task<UploaderResponse> Upload(ResidentialListingRequest request, CancellationToken cancellationToken = default);
         Task<int> DeleteDuplicateClients(Guid companyId, string mlsNumber, CancellationToken cancellationToken = default);
         void CancelOperation();
